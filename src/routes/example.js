8a82e99e07538b620cf0171a5182c7a4cada16ce
tubagusdylan <tubagus.dylanr@gmail.com>
2025-09-25 09:56:25 +0700

    fix: example modules

--- src/routes/example.js
+++ src/routes/example.js
@@ -1,9 +1,15 @@
-const customerHandler = require("../modules/customer/handlers/api_handler");
+// const customerHandler = require("../modules/customer/handlers/api_handler");
 
 module.exports = (server) => {
-  server.post("/codebase/v1/customer", customerHandler.addCustomer);
-  server.put("/codebase/v1/customer/:id", customerHandler.updateCustomer);
-  server.del("/codebase/v1/customer/:id", customerHandler.deleteCustomer);
-  server.get("/codebase/v1/customer/:id", customerHandler.getCustomerById);
-  server.get("/codebase/v1/customer", customerHandler.listCustomer);
+  // server.post("/codebase/v1/customer", customerHandler.addCustomer);
+  // server.put("/codebase/v1/customer/:id", customerHandler.updateCustomer);
+  // server.del("/codebase/v1/customer/:id", customerHandler.deleteCustomer);
+  // server.get("/codebase/v1/customer/:id", customerHandler.getCustomerById);
+  // server.get("/codebase/v1/customer", customerHandler.listCustomer);
+  server.get("/example", (req, res) => {
+    res.status(200).send({
+      data: "OK",
+      message: "Testing success"
+    })
+  })
 };
